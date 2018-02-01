188df70370d799ac178647aae92599e0750a55d7
itegrootenhuis <itegrootenhuis@bizstream.com>
2018-02-01 15:07:21 -0500

    lab 09 finish

---
+++ Lab09/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+    class Circle
+    {
+        private double radius;
+        private const double PI = Math.PI;
+
+        public Circle(double rad)
+        {
+            this.radius = rad;
+            CalculateCircumference(this.radius);
+            CalculateArea(this.radius);
+        }
+
+        public double CalculateArea(double radius)
+        {
+            double area = PI * (radius * radius);
+            return Math.Round(area, 2);
+        }
+
+        public double CalculateCircumference(double radius)
+        {
+            double circumference = 2 * (PI * radius);
+            return Math.Round(circumference, 2);
+        }
+    }
+}
--- Lab09/Program.cs
+++ Lab09/Program.cs
@@ -8,8 +8,47 @@ namespace Lab09
 {
     class Program
     {
+        public static int numOfCircles = 0;
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Circle area & circumference calculator. ");
+            GetUserInput();
+        }
+
+        private static void GetUserInput()
+        {
+            Console.Write("Enter a radius: ");
+            bool bit = double.TryParse(Console.ReadLine(), out double userInput);
+
+            if (bit)
+            {
+                numOfCircles++;
+                Circle circle = new Circle(userInput);
+                WriteResponse(circle.CalculateCircumference(userInput), circle.CalculateArea(userInput), numOfCircles);
+            }
+            else
+            {
+                Console.WriteLine("Your input was invalid.\n");
+                GetUserInput();
+            }
+        }
+
+        private static void WriteResponse(double circumference, double area, int numOfCircles)
+        {
+            Console.WriteLine("Circumference: \t{0} \nArea: \t{1} \nNumber of Circles: \t{2}", circumference, area, numOfCircles);
+            QuitConsoleApp();
+        }
+
+        private static void QuitConsoleApp()
+        {
+            Console.WriteLine("\n\nPress R to repeat or any other key to exit the app.");
+            ConsoleKeyInfo quitKey = Console.ReadKey();
+
+            if (quitKey.Key.ToString().ToLower() == "r")
+            {
+                Console.Clear();
+                GetUserInput();
+            }
         }
     }
 }
